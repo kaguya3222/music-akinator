835914ea6d498625205172abfe93a545096449be
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-19 18:20:22 +0200

    Changed main layout of app

---
+++ src/views/Main-Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app color="blue-grey" dark class="d-flex justify-center">
+      <v-toolbar-title>Music akinator</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content> </v-content>
+    <v-footer color="blue-grey" app>
+      <span class="white--text">&copy; BornedToCode 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
