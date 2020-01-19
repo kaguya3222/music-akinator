dbc45fb108cdadfa72b4f97418e71ff1238af786
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-19 18:59:30 +0200

    Created Name.vue

---
+++ src/components/Game/Name.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="d-flex flex-column">
+    <span>Введите свой никнейм:</span>
+    <v-text-field type="text" color="blue-grey" v-model="name"></v-text-field>
+    <v-btn class="align-self-center" color="blue-grey" dark @click="nextStep()"
+      >Играть</v-btn
+    >
+  </form>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      name: null
+    };
+  },
+  computed: {
+    ...mapGetters(["step"])
+  },
+  methods: {
+    ...mapActions(["increaseStep", "changeName"]),
+    nextStep() {
+      this.changeName({ payLoad: this.name });
+      this.increaseStep();
+    }
+  }
+};
+</script>
