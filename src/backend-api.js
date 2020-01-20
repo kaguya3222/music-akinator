7a8c83f8f6b39b045ca8cedfb3c48f6d44f2f5cd
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-20 20:48:08 +0200

    Created backend-api.js

---
+++ src/backend-api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default class API {
+  constructor({ songSearchEngineURL, token }) {
+    this.token = token;
+    this.songSearchEngineURL = songSearchEngineURL;
+  }
+
+  async findSongByLyrics({ lyrics }) {
+    return await axios.get(
+      `${this.songSearchEngineURL}?q=${lyrics}&api_token=${this.token}`
+    );
+  }
+
+  async findTrackId({ artist, title }) {
+    return await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=artist:"${artist}" track:"${title}"`
+    );
+  }
+}
