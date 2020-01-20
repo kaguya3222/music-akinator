e49bb81dc8a518b0c52b3b52317abc5ef5f2fa65
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-20 20:48:49 +0200

    Created Name.vue

--- src/components/Game/Name.vue
+++ src/components/Game/Name.vue
@@ -1,7 +1,12 @@
 <template>
   <form class="d-flex flex-column">
     <span>Введите свой никнейм:</span>
-    <v-text-field type="text" color="blue-grey" v-model="name"></v-text-field>
+    <v-text-field
+      type="text"
+      color="blue-grey"
+      v-model="name"
+      id="name"
+    ></v-text-field>
     <v-btn class="align-self-center" color="blue-grey" dark @click="nextStep()"
       >Играть</v-btn
     >
