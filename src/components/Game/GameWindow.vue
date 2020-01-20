9adcc62ddac40ce1da0f577997467c994aa7c750
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-20 20:48:38 +0200

    Created GameWindow.vue

--- src/components/Game/GameWindow.vue
+++ src/components/Game/GameWindow.vue
@@ -2,12 +2,14 @@
   <div class="d-flex justify-center align-center">
     <name v-if="step === 1"></name>
     <game v-if="step === 2"></game>
+    <result v-if="step === 3"></result>
   </div>
 </template>
 
 <script>
 import Name from "./Name";
 import Game from "./Game";
+import Result from "./Result";
 
 import { mapGetters } from "vuex";
 
@@ -20,7 +22,8 @@ export default {
   },
   components: {
     name: Name,
-    game: Game
+    game: Game,
+    result: Result
   },
   methods: {}
 };
