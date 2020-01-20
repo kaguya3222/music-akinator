21532bf3a0e581a5371212081cd4c4b57f570ccb
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-20 20:49:23 +0200

    Created Result.vue

---
+++ src/components/Game/Result.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Victory v-if="result === 'victory'"></Victory>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import Victory from "./Victory";
+
+export default {
+  components: {
+    Victory
+  },
+  computed: {
+    ...mapGetters(["result"])
+  }
+};
+</script>
