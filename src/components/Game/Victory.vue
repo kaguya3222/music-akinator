ab1f39bb34ca4e277337e21055c6b0029de8fab6
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-20 20:49:30 +0200

    Created Victory.vue

---
+++ src/components/Game/Victory.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-card class="d-flex flex-column align-center game-card px-2 py-2">
+      <p>Ура, я победил</p>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
