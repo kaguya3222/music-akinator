d43fe45cd5cfef5ce5064d9f198558211489e6b3
Andrew <andrewkarp00@gmail.com>
2020-01-20 21:51:14 +0200

    Created Defeat.vue

---
+++ src/components/Game/Defeat.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <v-card class="d-flex flex-column align-center game-card px-2 py-2">
+      <p>О нет, я проиграл :(</p>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
