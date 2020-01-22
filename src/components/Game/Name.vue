37fd33dbfb30bfd1fcee2d135ecc2956679f0759
tezzi2001 <bohdan.bondarenko2001@gmail.com>
2020-01-22 21:54:24 +0200

    Added validation for Name.vue

--- src/components/Game/Name.vue
+++ src/components/Game/Name.vue
@@ -1,21 +1,33 @@
 <template>
-  <form class="d-flex flex-column">
+  <v-form class="d-flex flex-column">
     <v-text-field
       type="text"
       color="blue-grey"
-      v-model="name"
+      v-model="$v.name.$model"
       id="name"
       label="Введите никнейм"
+      :error-messages="nameErrors"
+      :success="!$v.name.$invalid"
     ></v-text-field>
-    <v-btn class="align-self-center" color="blue-grey" dark @click="nextStep()"
+    <v-btn
+      class="align-self-center mt-2"
+      color="blue-grey"
+      :dark="!isDisabled"
+      @click="nextStep()"
+      :disabled="isDisabled"
       >Играть</v-btn
     >
-  </form>
+  </v-form>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import { mapActions } from "vuex";
+
+import { required, maxLength } from "vuelidate/lib/validators";
+
+import validationHelpers from "../../mixins/validationHelpers";
+import { validationMixin } from "vuelidate";
 
 export default {
   data() {
@@ -24,7 +36,30 @@ export default {
     };
   },
   computed: {
-    ...mapGetters(["step"])
+    ...mapGetters(["step"]),
+    nameErrors() {
+      let errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      errors = this.checkIfErrorShouldAppend({
+        field: this.$v.name,
+        errors,
+        errorMessage: "Это поле обязательно!",
+        errorType: "required"
+      });
+      errors = this.checkIfErrorShouldAppend({
+        field: this.$v.name,
+        errors,
+        errorMessage: "Максимальная длинна никнейма 10 символов!",
+        errorType: "maxLength"
+      });
+      return errors;
+    },
+    isDisabled() {
+      return this.$v.$invalid;
+    }
+  },
+  validations: {
+    name: { required, maxLength: maxLength(10) }
   },
   methods: {
     ...mapActions(["increaseStep", "changeName"]),
@@ -32,6 +67,7 @@ export default {
       this.changeName({ payLoad: this.name });
       this.increaseStep();
     }
-  }
+  },
+  mixins: [validationHelpers, validationMixin]
 };
 </script>
