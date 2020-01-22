0d6a1c9a39b45cf6e978eb916796406be83084bd
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-22 20:52:42 +0200

    Modified Name.vue with label attribute

--- src/components/Game/Name.vue
+++ src/components/Game/Name.vue
@@ -1,11 +1,11 @@
 <template>
   <form class="d-flex flex-column">
-    <span>Введите свой никнейм:</span>
     <v-text-field
       type="text"
       color="blue-grey"
       v-model="name"
       id="name"
+      label="Введите никнейм"
     ></v-text-field>
     <v-btn class="align-self-center" color="blue-grey" dark @click="nextStep()"
       >Играть</v-btn
