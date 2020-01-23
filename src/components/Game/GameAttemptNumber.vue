7405ae267e0574d4109276d4c8dbcee5d1b69705
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-24 01:51:10 +0200

    Created GameAttemptNumber.vue

---
+++ src/components/Game/GameAttemptNumber.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex full-width">
+    <span>{{ name }}</span>
+    <span class="ml-auto">Попытка {{ attemptNumber }}</span>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["name", "attemptNumber"])
+  }
+};
+</script>
