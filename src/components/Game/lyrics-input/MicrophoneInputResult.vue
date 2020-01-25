c1d28356082c0831d2214138c1ff7c1eea4beb6a
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-25 04:41:09 +0200

    Created MicrophoneInputResult.vue

---
+++ src/components/Game/lyrics-input/MicrophoneInputResult.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    v-show="this.lyrics"
+    class="d-flex flex-column align-center"
+    v-if="isSpeechLangChosen"
+  >
+    <p>Вы напели:</p>
+    <p>{{ this.lyrics }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      recognitionEngine: null
+    };
+  },
+  computed: {
+    ...mapGetters(["lyrics", "isSpeechLangChosen"])
+  }
+};
+</script>
