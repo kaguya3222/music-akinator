817337ca7071a114a9585d5de31f98aed8cba0f0
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-25 04:38:31 +0200

    Created LyricsGetAnswerButton.vue

---
+++ src/components/Game/lyrics-input/LyricsGetAnswersButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="d-flex justify-center mt-1 mb-1">
+    <v-btn
+      v-show="this.lyrics"
+      @click="getAnswers({ lyrics })"
+      color="blue-grey"
+      :max-width="100"
+      dark
+      >Загадать</v-btn
+    >
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+import API from "../../../backend-api";
+
+import parsingHelpers from "../../../mixins/parsingHelpers";
+
+export default {
+  data() {
+    return {
+      searchEngine: new API({
+        songSearchEngineURL: `https://cors-anywhere.herokuapp.com/https://api.audd.io/findLyrics/`,
+        token: "6586db1d822b505cc809c62c9c27febb"
+      })
+    };
+  },
+  computed: {
+    ...mapGetters(["lyrics", "answers", "attemptNumber"])
+  },
+  methods: {
+    ...mapActions(["setAnswers", "changeAnswersReceivedStatus"]),
+    async getAnswers({ lyrics }) {
+      this.changeAnswersReceivedStatus({ payLoad: false });
+      lyrics = this.removeLinebreaks({ str: lyrics });
+      const response = await this.searchEngine.findSongByLyrics({ lyrics });
+      this.changeAnswersReceivedStatus({ payLoad: true });
+      if (response.data.result.length === 0) {
+        return true;
+      }
+      this.setAnswers({
+        payLoad: this.removeDuplicates({
+          array: this.handleAnswers({ answers: response })
+        })
+      });
+      this.setAttemptsQuantity();
+    },
+    handleAnswers({ answers }) {
+      return answers.data.result.map(el => {
+        el.title = this.parseRussianTitle({ title: el.title }).trim();
+        return {
+          artist: el.artist,
+          title: el.title
+        };
+      });
+    },
+    setAttemptsQuantity() {
+      if (this.answers.length < 5)
+        this.$store.dispatch("changeAttemptsQuantity", {
+          payLoad: this.answers.length
+        });
+    }
+  },
+  mixins: [parsingHelpers]
+};
+</script>
