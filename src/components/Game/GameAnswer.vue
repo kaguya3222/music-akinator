c22c7ac3cee6a712466d3dd623fd19c0b29b3e26
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-25 04:49:22 +0200

    Created GameAnswer.vue

---
+++ src/components/Game/GameAnswer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="answers.length" class="d-flex flex-column full-width">
+    <answer-track-name></answer-track-name>
+    <answer-buttons></answer-buttons>
+  </div>
+</template>
+
+<script>
+import AnswerTackName from "./answers/AnswerTrackName";
+import AnswerButtons from "./answers/AnswerButtons";
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["answers"])
+  },
+  components: {
+    "answer-track-name": AnswerTackName,
+    "answer-buttons": AnswerButtons
+  }
+};
+</script>
