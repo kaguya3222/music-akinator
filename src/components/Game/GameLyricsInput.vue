b597111b7d1059215550092335ecff74244faca5
kaguya3222 <maksimanisimov322@gmail.com>
2020-01-25 04:17:38 +0200

    Created GameLyricsInput.vue

--- src/components/Game/GameLyricsInput.vue
+++ src/components/Game/GameLyricsInput.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="mt-2">
-    <span>Выберите тип ввода слов из песни:</span>
-    <div class="d-flex justify-center align-center full-width">
-      <v-btn
-        text
-        icon
-        color="blue-grey"
-        :x-large="true"
-        @click="changeLyricsInputType({ payLoad: 'micro' })"
-        ><v-icon>mdi-microphone</v-icon></v-btn
-      >
-      <v-btn
-        text
-        icon
-        color="blue-grey"
-        :x-large="true"
-        @click="changeLyricsInputType({ payLoad: 'keyboard' })"
-        ><v-icon>mdi-keyboard</v-icon></v-btn
-      >
-    </div>
+  <div class="mt-2" :key="lyricsInputType" v-if="!answers.length">
+    <lyrics-input-options></lyrics-input-options>
+    <lyrics-text-field-input></lyrics-text-field-input>
+    <lyrics-microphone-input></lyrics-microphone-input>
+    <lyrics-get-answers-button></lyrics-get-answers-button>
+    <lyrics-not-found-warning></lyrics-not-found-warning>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+
+import LyricsInputOptions from "./lyrics-input/LyricsInputOptions";
+import LyricsTextFieldInput from "./lyrics-input/LyricsTextFieldInput";
+import LyricsMicrophoneInput from "./lyrics-input/LyricsMicrophoneInput";
+import LyricsGetAnswersButton from "./lyrics-input/LyricsGetAnswersButton";
+import LyricsNotFoundWarning from "./lyrics-input/LyricsNotFoundWarning";
 
 export default {
-  methods: {
-    ...mapActions(["changeLyricsInputType"])
+  computed: {
+    ...mapGetters(["lyricsInputType", "answers"])
+  },
+  components: {
+    "lyrics-text-field-input": LyricsTextFieldInput,
+    "lyrics-microphone-input": LyricsMicrophoneInput,
+    "lyrics-get-answers-button": LyricsGetAnswersButton,
+    "lyrics-input-options": LyricsInputOptions,
+    "lyrics-not-found-warning": LyricsNotFoundWarning
+  },
+  updated() {
+    this.$store.dispatch("resetSpeechLanguage");
+    this.$store.dispatch("setLyrics", { payLoad: null });
   }
 };
 </script>
