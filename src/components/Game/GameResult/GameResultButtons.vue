40d9dfdc8ae01b76c1e47b59293850d9f2d02d7a
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-22 10:55:30 +0200

    Added tooltips for GameResultButtons.vue

--- src/components/Game/GameResult/GameResultButtons.vue
+++ src/components/Game/GameResult/GameResultButtons.vue
@@ -1,32 +1,50 @@
 <template>
   <div class="d-flex justify-center align-center full-width">
-    <v-btn
-      text
-      icon
-      color="blue-grey"
-      :x-large="true"
-      @click="replay()"
-      v-show="showButton"
-      ><v-icon>mdi-replay</v-icon></v-btn
-    >
-    <v-btn
-      text
-      icon
-      color="blue-grey"
-      :x-large="true"
-      @click="toggleShowSuggestedAnswers"
-      v-show="showButton"
-      ><v-icon>mdi-format-list-bulleted</v-icon></v-btn
-    >
-    <v-btn
-      text
-      icon
-      color="blue-grey"
-      :x-large="true"
-      @click="exit()"
-      v-show="showButton"
-      ><v-icon>mdi-exit-to-app</v-icon></v-btn
-    >
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          icon
+          color="blue-grey"
+          :x-large="true"
+          v-on="on"
+          @click="replay()"
+          v-show="showButton"
+          ><v-icon>mdi-replay</v-icon></v-btn
+        >
+      </template>
+      <span>Играть еще</span>
+    </v-tooltip>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          icon
+          color="blue-grey"
+          :x-large="true"
+          v-on="on"
+          @click="toggleShowSuggestedAnswers"
+          v-show="showButton"
+          ><v-icon>mdi-format-list-bulleted</v-icon></v-btn
+        >
+      </template>
+      <span>Список всех треков</span>
+    </v-tooltip>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          icon
+          color="blue-grey"
+          :x-large="true"
+          v-on="on"
+          @click="exit()"
+          v-show="showButton"
+          ><v-icon>mdi-exit-to-app</v-icon></v-btn
+        >
+      </template>
+      <span>Закончить игру</span>
+    </v-tooltip>
   </div>
 </template>
 
